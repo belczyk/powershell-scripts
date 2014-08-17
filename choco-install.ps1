b41fc7ef2cc78fef44022bc0b20c57544b50cfe5
Sebastian Belczyk <sbelczyk@gmail.com>
2014-08-17 18:04:57 +1200

    add choco install script

---
+++ choco-install.ps1
@@ -0,0 +1,22 @@
+choco install GoogleChrome
+choco install ditto
+choco install launchy
+choco install sublimetext2
+choco install SublimeText2.PackageControl -Version 1.6.3
+choco install IrfanView
+choco install thunderbird 
+choco install 7zip
+choco install nodejs.install
+choco install fiddler4 
+choco install winscp
+choco install f.lux
+choco install iTunes
+choco install vlc
+
+#blocking
+#choco install avastfreeantivirus
+#choco install SourceTree
+#choco install ColorPic
+
+#dependent on other stuff
+#choco install resharper
