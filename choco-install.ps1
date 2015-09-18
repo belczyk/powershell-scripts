0697447a0653e7b21aefc0588351354877cd62a7
Sebastian Belczyk <sbelczyk@gmail.com>
2015-09-18 19:11:25 +0200

    Update choco-install.ps1
    update with new tools

--- choco-install.ps1
+++ choco-install.ps1
@@ -5,11 +5,16 @@ choco install sublimetext3.app -y
 choco install sublimetext3.packagecontrol -y
 choco install IrfanView -y
 choco install 7zip -y
-#choco install nodejs.install -y
+choco install nodejs
 choco install fiddler4  -y
 choco install winscp -y
 choco install f.lux -y
-choco install iTunes -y
+choco install consolez
+choco install adobereader
+choco install greenshot
+choco install firefox
+
+#choco install iTunes -y
 choco install vlc -y
 choco install skype -y
 #blocking
